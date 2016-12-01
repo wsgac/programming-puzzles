46d8d578dc5a1022f798cf347a7fbec9dd2c7fda
Wojciech Gac <wgac@ravenpack.com>
2016-12-01 16:05:36 +0100

    Puzzle 1 - Part 2

--- puzzle1.el
+++ puzzle1.el
@@ -31,6 +31,10 @@
 ;; Position relative to start. car - x, cdr - y
 (defvar *position* (cons 0 0))
 
+;;;;;;;;;;;;
+;; Part 1 ;;
+;;;;;;;;;;;;
+
 (defun change-azimuth (rotation)
   (cond
    ((member rotation '(r R "r" "R"))
@@ -45,14 +49,29 @@
          (direction-value (string-to-number (substring direction-string 1))))
     (cons direction-symbol direction-value)))
 
+(defun produce-interval (a b)
+  (if (< a b)
+      (cl-loop for i from (1+ a) to b collect i)
+    (cl-loop for i downfrom (1- a) to b collect i)))
+
 (defun change-position (direction)
-  (let ((dir (split-direction direction)))
+  (let ((dir (split-direction direction))
+        (old-pos (copy-list *position*)))
     (change-azimuth (car dir))
     (if (zerop (mod *azimuth* 2))
         ;; y-direction
         (incf (cdr *position*) (* (- 1 *azimuth*) (cdr dir)))
       ;; x-direction
-      (incf (car *position*) (* (- 2 *azimuth*) (cdr dir))))))
+      (incf (car *position*) (* (- 2 *azimuth*) (cdr dir))))
+    (message "Old: %s New: %s" old-pos *position*)
+    (cond
+     ((= (car old-pos) (car *position*))
+      (mapcar (lambda (y) (cons (car *position*) y))
+              (produce-interval (cdr old-pos) (cdr *position*))))
+     ((= (cdr old-pos) (cdr *position*))
+      (mapcar (lambda (x) (cons x (cdr *position*)))
+              (produce-interval (car old-pos) (car *position*))))
+     (t (error "Invalid position change: %s" direction)))))
 
 (defun solve-puzzle (&optional directions)
   ;; reset *position* and *azimuth*
@@ -65,6 +84,28 @@
 
 (defun test-solution ()
   (and
-   (= (solve-puzzle '(r2 l3)) 5)
+   (= (solve-puzzle '(rpp2 l3)) 5)
    (= (solve-puzzle '(r2 r2 r2)) 2)
    (= (solve-puzzle '(r5 l5 r5 r3)) 12)))
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+(defun solve-puzzle-2 (&optional directions)
+  (setf *azimuth* 0
+        *position* (cons 0 0))
+  (cl-loop
+   with positions = nil
+   for direction in (or directions *directions*)
+   for position-list = (change-position direction)
+   append position-list into positions
+   finally (return (cl-loop
+                    for i on positions
+                    when (find (car i) (cdr i) :test #'equal)
+                    do (return (+ (abs (car (car i)))
+                                  (abs (cdr (car i)))))))))
+
+(defun test-solution-2 ()
+  (and
+   (= (solve-puzzle-2 '(r8 r4 r4 r8)) 4)))
